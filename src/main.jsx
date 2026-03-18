81e183cc4ff4450256426805be9209d19c682573
kdshah50 <90711671+kdshah50@users.noreply.github.com>
2026-03-18 19:14:33 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Tianguis from '../naranjogo.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Tianguis />
+  </StrictMode>
+)
